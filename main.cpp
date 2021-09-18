a2c6513af80536afc60e3c1c607cec9fe9b4715d
Caitlin Wong <cylwong03@gmail.com>
2021-09-18 19:54:20 -0400

    Added success function to determine pass or fail.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <sstream>
+using namespace std ;
+
+bool success( unsigned int num_words, int num_typed);
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+
+
+	cout << "Hello World!" << endl;
 	return 0;
+}
+
+bool success( unsigned int num_words, unsigned int num_typed) {
+
+	if (num_words > num_typed) {
+		cout << "Sorry, you typed " << num_typed << " words, and you needed " << num_words << ". Try again!" << endl;
+	}
+	else {
+		cout << "You typed " << num_typed << " words, and you needed " << num_words << ". Great job! Starting next level..." << endl;
+	}
+
+
 }
