339773b4bb8e18d2cc7b91aaa874941fdcca9861
captainlide <captainlide@gmail.com>
2021-09-19 02:58:00 -0400

    changed callwords.py

--- callwords.py
+++ callwords.py
@@ -1,7 +1,12 @@
-import os 
-cmd = 'curl https://random-word-api.herokuapp.com/word?number=20'
-words = []
+#import os 
+#cmd = 'curl https://random-word-api.herokuapp.com/word?number=20'
+#words = []
 
-for word in os.system(cmd):
-    words.append(word)
+#for word in os.system(cmd):
+#    words.append(word)
+#print(words)
+
+import subprocess
+cmd = "curl -s https://random-word-api.herokuapp.com/word?number=20"
+words = subprocess.check_output(cmd, shell=True).rstrip()
 print(words)
