88fe548468a38754c5703ff41498da048b59b41b
Elina Zheng <47897713+elinxaaa@users.noreply.github.com>
2021-09-19 03:11:38 -0400

    implement word bank

--- callwords.py
+++ callwords.py
@@ -1,12 +1,4 @@
-#import os 
-#cmd = 'curl https://random-word-api.herokuapp.com/word?number=20'
-#words = []
-
-#for word in os.system(cmd):
-#    words.append(word)
-#print(words)
-
 import subprocess
 cmd = "curl -s https://random-word-api.herokuapp.com/word?number=20"
 words = subprocess.getoutput(cmd)
-print(words)
+
---
+++ main.py
@@ -0,0 +1,2 @@
+from callwords import words
+
